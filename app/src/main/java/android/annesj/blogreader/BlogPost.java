76f7afb46029d957f54bc2aefbfe32be15240a0a
jimmy annes <jimmyannes98@gmail.com>
2014-10-02 13:52:52 -0700

    milestone 06

---
+++ app/src/main/java/android/annesj/blogreader/BlogPost.java
@@ -0,0 +1,10 @@
+package android.annesj.blogreader;
+
+public class BlogPost {
+    public final String title;
+    public final String url;
+
+    public BlogPost(String title, String url){
+        
+    }
+}
