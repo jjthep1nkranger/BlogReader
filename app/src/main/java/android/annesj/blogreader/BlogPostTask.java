ac2c308d54b61b18fac1689edbf3fca8e4db85ac
jimmy annes <jimmyannes98@gmail.com>
2014-10-18 22:50:00 -0700

    no message

--- app/src/main/java/android/annesj/blogreader/BlogPostTask.java
+++ app/src/main/java/android/annesj/blogreader/BlogPostTask.java
@@ -44,9 +44,10 @@ public class BlogPostTask extends AsyncTask<Activity, Void, JSONObject>{
 
     @Override
     protected void onPostExecute(JSONObject jsonObject) {
+        BlogPostParser.get().reedFeed(jsonObject);
         ListView listView = (ListView)activity.findViewById(R.id.listview);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(activity, android.R.layout.simple_list_item_1, arrayStrings);
+        ArrayAdapter<BlogPost> adapter = new ArrayAdapter<BlogPost>(activity, android.R.layout.simple_list_item_1, BlogPostParser.get().posts);
         listView.setAdapter(adapter);
     }
 }
