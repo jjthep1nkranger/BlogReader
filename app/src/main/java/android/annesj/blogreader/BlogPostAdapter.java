60ba6ec3d7b4edc3ce5ef9e1235eee39960a15b3
jimmy annes <jimmyannes98@gmail.com>
2014-10-20 14:21:55 -0700

    s32

--- app/src/main/java/android/annesj/blogreader/BlogPostAdapter.java
+++ app/src/main/java/android/annesj/blogreader/BlogPostAdapter.java
@@ -1,7 +1,11 @@
 package android.annesj.blogreader;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,5 +13,19 @@ import java.util.List;
 public class BlogPostAdapter extends ArrayAdapter<BlogPost> {
     public BlogPostAdapter(Context context, ArrayList<BlogPost> posts) {
         super(context, 0, posts);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        BlogPost post = getItem(position);
+
+        if (convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        }
+
+        TextView textView = (TextView)convertView.findViewById(android.R.id.text1);
+        textView.setText(post.title);
+
+        return convertView;
     }
 }
